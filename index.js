a4689ff836fa6da46a56a517a2641c4ae0699413
Saiful Islam <saiful islam>
2024-04-28 20:15:28 +0600

    connect server to database

--- index.js
+++ index.js
@@ -6,20 +6,21 @@ const app = express();
 const port = process.env.PROT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173", "https://sparkling-paletas-aeb127.netlify.app/"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    withCredentials: true,
+}
+))
 app.use(express.json())
 
 
-
-
 app.get('/', (req, res) => {
-    res.send('')
+    res.send('art and craft')
 })
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xes5bsh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -33,6 +34,18 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+
+        const productCollection = client.db('Art&CraftStoreDB').collection('product')
+
+        app.get()
+
+        app.post('/addCraft', async (req, res) => {
+            const product = req.body
+            console.log('new product', product);
+            const result = await productCollection.insertOne(product)
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
