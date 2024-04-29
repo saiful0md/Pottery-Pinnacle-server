e7c06974baca997583a6d70ac3a8a6b3e0a4637b
Saiful Islam <saiful islam>
2024-04-29 10:40:59 +0600

    create post methode

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
 const app = express();
@@ -38,7 +38,36 @@ async function run() {
 
         const productCollection = client.db('Art&CraftStoreDB').collection('product')
 
-        app.get()
+        app.get('/myArtAndCraft/:email', async (req, res) => {
+            const result = await productCollection.find({ email: req.params.email }).toArray()
+            res.send(result)
+        })
+        app.get('/singleProduct/:id', async (req, res) => {
+            console.log(req.params.id);
+            const result = await productCollection.findOne({ _id: new ObjectId(req.params.id) });
+            res.send(result)
+        })
+        app.put('/update/:id', async (req, res) => {
+            console.log(req.params.id);
+            const query = { _id: new ObjectId(req.params.id) };
+            const options = { upsert: true };
+            const getUpdateInfo = req.body
+            const updatedProductInfo = {
+                $set: {
+                    photoUrl: getUpdateInfo.photoUrl,
+                    itemName: getUpdateInfo.itemName,
+                    subCategory: getUpdateInfo.subCategory,
+                    description: getUpdateInfo.description,
+                    price: getUpdateInfo.price,
+                    rating: getUpdateInfo.rating,
+                    customization: getUpdateInfo.customization,
+                    processing: getUpdateInfo.processing,
+                    stockStatus: getUpdateInfo.stockStatus,
+                }
+            };
+            const result = await productCollection.updateOne(query, updatedProductInfo, options);
+            res.send(result)
+        })
 
         app.post('/addCraft', async (req, res) => {
             const product = req.body
